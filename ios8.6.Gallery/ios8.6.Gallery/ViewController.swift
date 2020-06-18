783728933b8abc2a9dbca913dc1c21750e4a3d4e
Rinat Sagitov <sagitovrf@gmail.com>
2020-06-18 16:41:56 +0500

    Add Gallery 8.6 Homework

---
+++ ios8.6.Gallery/ios8.6.Gallery/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  ios8.6.Gallery
+//
+//  Created by user on 18.06.2020.
+//  Copyright © 2020 user. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var images: [UIImage] = []
+    var counter: Int = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        for i in 1...10 {
+            images.append(UIImage(imageLiteralResourceName: "Image\(i)"))
+        }
+        imageView.image = images.first
+    }
+
+    @IBAction func nextButton(_ sender: Any) {
+        counter = (counter + 1) % images.count
+        imageView.image = images[counter]
+    }
+    @IBAction func prevButton(_ sender: Any) {
+        counter = counter - 1 < 0 ? 0 : (counter - 1) % images.count
+        imageView.image = images[counter]
+    }
+}
+
